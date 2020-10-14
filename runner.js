a8edf434cacbf095f68e6ca1051cf7f9012d07d1
Gregg Tavares <github@greggman.com>
2020-10-15 04:01:55 +0900

    try html blob

--- runner.js
+++ runner.js
@@ -3,6 +3,11 @@
     type: 'gimmeDaCodez',
   }, "*");
 
+  // forward messages from iframe to parent
+  window.addEventListener('message', (e) => {
+    window.parent.postMessage(e.data, "*");
+  });
+
   function find(files, endings) {
     // calling toLowerCase a bunch is bad but there will never be more than a few files
     for (const ending of endings) {
@@ -31,14 +36,43 @@
 
   function insertInBlob(mainHTML, mainJS, mainCSS) {
     const style = document.createElement('style');
-    style.textContent = mainCSS.content;
-    (document.head || document.body || document.documentElement).appendChild(style);
-    document.body.innerHTML = mainHTML.content;
-    const blob = new Blob([mainJS.content], {type: 'application/javascript'});
-    const script = document.createElement('script');
-    script.type = 'module';
-    script.src = URL.createObjectURL(blob);
-    document.body.appendChild(script);
+    style.textContent = `
+html, body, iframe {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+iframe {
+  border: none;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+`;
+    document.head.appendChild(style);
+    const iframe = document.createElement('iframe');
+    const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>jsGist runner frame</title>
+    <style>
+${mainCSS.content}
+    </style>
+    <script src="https://jsgistrunner.devcomments.org/helper.js" type="module"></script>
+  </head>
+  <body>
+${mainHTML.content}
+  </body>
+  <${'script'}> type="module">
+${mainJS.content}
+  </${'script'}>
+</html>
+`;
+    const blob = new Blob([html], {type: 'text/html'});
+    document.body.appendChild(iframe);
+    iframe.src = URL.createObjectURL(blob);
   }
 
   const handlers = {
